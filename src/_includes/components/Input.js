576de42e2851f146a4367d5a4a3b0abfb41ab7c7
k.dubov <example@example.com>
2022-11-23 17:55:00 +0300

    Add Email page

--- src/_includes/components/Input.js
+++ src/_includes/components/Input.js
@@ -23,4 +23,32 @@ const Input = ({ type, id, label, children = "", rest = {} }) => {
 `
 };
 
-module.exports = Input;
+const Checkbox = ({ id, label }) => {
+    return html`
+        <div class="form-group">
+            <label class="form-checkbox">
+                <input type="checkbox" id="${id}" name="${id}">
+                <svg
+                    width="32" height="32" viewbox="-4 -4 39 39" aria-hidden="true" focusable="false">
+                    <!-- The background -->
+                    <rect
+                        class="checkbox__background"
+                        width="35"
+                        height="35"
+                        x="-2"
+                        y="-2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-width="3"
+                        rx="6"
+                        ry="6"></rect>
+                    <!-- The checkmark-->
+                    <polyline class="checkbox__checkmark" points="4,14 12,23 28,5" stroke="transparent" stroke-width="4" fill="none"></polyline>
+                </svg>
+                <span>${label}</span>
+            </label>
+        </div>
+`
+};
+
+module.exports = { Input, Checkbox };
