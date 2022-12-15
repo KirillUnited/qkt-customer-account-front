697b60adc3a838274d0ad811c540caa591a58d99
k.dubov <example@example.com>
2022-12-15 17:57:59 +0300

    Add Ticket/Booking Details page

---
+++ src/_data/ticket_details.js
@@ -0,0 +1,20 @@
+module.exports = {
+    path: "/pages/ticket-details",
+    title: "Rocking The Daisies",
+    image: "ticket-details-banner.png",
+    tag: "UPCOMING",
+    status: "primary",
+    desc: {
+        "Date": "08:00 Friday 5 October 2018 - 16:00 Sunday 7 October 2018",
+        "Reference Number": "REF02253665",
+        "Number Of Tickets": 6,
+        "Amount": "R2,570",
+    },
+    actions: [
+        {
+            title: "Edit Ticket Details",
+            icon: "",
+            link: "/"
+        }
+    ]
+};
