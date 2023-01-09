f82bb6233cab278e2a3f46c8e0ee11b53ede7efe
k.dubov <example@example.com>
2023-01-09 20:52:11 +0300

    Add Earn Comission page

---
+++ src/_data/earn_comission.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "title": "Earn Comission",
+    "path": "/pages/earn-comission",
+    "actions": [
+        {
+            "img": "icons/Megaphone.svg",
+            "title": "Promoter Networks",
+            "desc": "Promote events for organisers.",
+            "link": "/pages/earn-comission/networks"
+        },
+        {
+            "img": "icons/link.svg",
+            "title": "Referrals",
+            "desc": "Sign event organisers up to quicket",
+            "link": "/pages/earn-comission/referrals"
+        }
+    ]
+};
