95f8336815705ee393a134b727e3589257cd33c1
k.dubov <example@example.com>
2023-01-11 09:22:50 +0300

    Add Promoter Networks - Empty page

--- src/_data/earn_comission.js
+++ src/_data/earn_comission.js
@@ -1,18 +1,34 @@
 module.exports = {
     "title": "Earn Comission",
     "path": "/pages/earn-comission",
-    "actions": [
-        {
-            "img": "icons/Megaphone.svg",
-            "title": "Promoter Networks",
-            "desc": "Promote events for organisers.",
-            "link": "/pages/earn-comission/networks"
-        },
-        {
-            "img": "icons/link.svg",
-            "title": "Referrals",
-            "desc": "Sign event organisers up to quicket",
-            "link": "/pages/earn-comission/referrals"
-        }
-    ]
+    "actions": {
+        "networks":
+            {
+                "img": "icons/Megaphone.svg",
+                "title": "Promoter Networks",
+                "desc": "Promote events for organisers.",
+                "link": "/pages/earn-comission/networks",
+                "placeholder": {
+                    "img": "/assets/images/earn-comission/placeholder-networks.png",
+                    "title": "Become a promoter",
+                    "desc": "You join promoter networks by invitation from an organiser or by browsing public promoter networks.",
+                    "footer": null,
+                    "link": "/pages/earn-comission/networks-empty"
+                }
+            },
+        "referrals":
+            {
+                "img": "icons/link.svg",
+                "title": "Referrals",
+                "desc": "Sign event organisers up to quicket",
+                "link": "/pages/earn-comission/referrals",
+                "placeholder": {
+                    "img": "/assets/images/earn-comission/placeholder-referrals.png",
+                    "title": "No Comissions Earned Yet",
+                    "desc": "Your commissions will appear here once they go through.",
+                    "footer": null,
+                    "link": "/pages/earn-comission/referrals-empty"
+                }
+            }
+    }
 };
