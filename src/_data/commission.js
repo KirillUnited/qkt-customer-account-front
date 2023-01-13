254d1381bbf2099106b20fe6acd2428a8e6315e6
k.dubov <example@example.com>
2023-01-13 16:20:52 +0300

    [Earn Commission] Add Promoter Networks page

--- src/_data/commission.js
+++ src/_data/commission.js
@@ -8,6 +8,30 @@ module.exports = {
                 "title": "Promoter Networks",
                 "desc": "Promote events for organisers.",
                 "link": "/pages/earn-commission/networks",
+                content: [
+                    {
+                        "title": "St Andrew’s Family Festival",
+                        "img": "tickets/ticket-1.png",
+                        "desc": "Share this link to earn 1.00% of ticket sales:" +
+                            "<br/><strong>https://qkt.io/zxB5HP</strong>",
+                        "tag": "UPCOMING",
+                        "status": "primary"
+                    },
+                    {
+                        "title": "Rocking The Daisies 2",
+                        "img": "tickets/ticket-2.png",
+                        "desc": "<strong>Ticket Holder:</strong> John Doe" +
+                            "<br/><strong>Date:</strong> Sunday, 7 October 2018" +
+                            "<br/><strong>Bank Details:</strong> FNB Savings **** 7682"
+                    },
+                    {
+                        "title": "Rocking The Daisies 3",
+                        "img": "tickets/ticket-3.png",
+                        "desc": "<strong>Ticket Holder:</strong> John Doe" +
+                            "<br/><strong>Date:</strong> Sunday, 7 October 2018" +
+                            "<br/><strong>Bank Details:</strong> FNB Savings **** 7682"
+                    }
+                ],
                 "placeholder": {
                     "img": "/assets/images/earn-commission/placeholder-networks.png",
                     "title": "Become a promoter",
