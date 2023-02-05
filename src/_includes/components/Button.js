458a796af663aee23afc68327fbb3f469bac24a9
k.dubov <example@example.com>
2023-02-05 20:49:41 +0300

    [Browse Events] add products filter custom date

--- src/_includes/components/Button.js
+++ src/_includes/components/Button.js
@@ -1,9 +1,9 @@
 const {html} = require("common-tags");
-const Button = ({children, state = null, type = "button", rest = {}}) => {
+const Button = ({children="Button", state = null, type = "button", rest = {}}) => {
     return html`
         <button class='btn ${state ? "btn-" + state : ""}' type="${type}" ${state === "disabled" ? state : ""} ${rest.toggle && `data-toggle="${rest.toggle}"`}
          ${rest.target && `data-target="${rest.target}"`}>
-            ${children || "Button"}
+            ${children}
         </button>
     `;
 };
---
+++ src/assets/js/products/ProductsFilterCustomDate.js
@@ -0,0 +1,73 @@
+import { html } from "common-tags";
+import Datepicker from "../../vendors/Datepicker.js/datepicker";
+
+export default class ProductsFilterCustomDate {
+    config = {
+        multiple: true,
+        inline: true,
+        classNames: {
+            node: 'datepicker custom hidden'
+        },
+
+        templates: {
+            container: [
+                '<div class="datepicker__container">',
+                '<div class="datepicker__pane">',
+                '<%= renderHeader() %>',
+                '<%= renderCalendar() %>',
+                this.getFooter(),
+                '</div>',
+                '</div>'
+            ].join('')
+        }
+    };
+    handleClick = (e) => {
+        const saveButton = e.target.closest('[data-filter-date-save="true"]');
+        const resetButton = e.target.closest('[data-filter-date-reset="true"]');
+
+        if (saveButton) {
+            this.hide();
+        }
+        if (resetButton) {
+            this.reset();
+        }
+    }
+
+    constructor(selector) {
+        this.selector = selector;
+        this.datepicker = new Datepicker(this.selector, this.config);
+        this.customDate = document.querySelector('.datepicker');
+
+        this.customDate.addEventListener("click", this.handleClick);
+    }
+
+    show() {
+        this.customDate.classList.remove('hidden');
+    }
+
+    hide() {
+        this.customDate.classList.add('hidden');
+    }
+
+    reset() {
+        const selectedDates = this.customDate.querySelectorAll('.is-selected');
+        
+        this.datepicker.setDate([]);
+        selectedDates.forEach(item=>{
+            item.classList.remove('is-selected')
+        });
+    }
+
+    getFooter() {
+        return html`
+            <div class="datepicker-footer">
+                <button class="btn" type="button" data-filter-date-reset="true">
+                    Clear Dates
+                </button>
+                <button class="btn btn-primary" type="button" data-filter-date-save="true">
+                    save
+                </button>
+            </div>
+            `;
+    }
+}
