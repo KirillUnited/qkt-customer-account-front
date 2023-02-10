c989ecbc46bce3c4d0c7cbe58c024c781af09c25
k.dubov <example@example.com>
2023-02-10 10:56:24 +0300

    refact: [catalog] split products scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 
-module.exports={
+module.exports = {
     entry: {
-        main: path.resolve(__dirname, "src/assets/js", "index.js")
+        main: path.resolve(__dirname, "src/assets/js", "index.js"),
+        products: path.resolve(__dirname, "src/assets/js", "products.js")
     },
     output: {
         filename: "[name].bundle.js",
