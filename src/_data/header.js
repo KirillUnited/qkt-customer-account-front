b5d257978045349400261b0da6cad0f148b13641
k.dubov <example@example.com>
2023-02-22 16:38:04 +0300

    feat: [header] add data file

---
+++ src/_data/header.js
@@ -0,0 +1,71 @@
+module.exports = {
+    "menu": {
+        "products": {
+            "title": "products",
+            "header": null,
+            "list": [
+                {
+                    "title": "Ticketing",
+                    "link": ""
+                },
+                {
+                    "title": "Online Event",
+                    "link": ""
+                },
+                {
+                    "title": "Fundraisers",
+                    "link": ""
+                },
+                {
+                    "title": "Subscriptions",
+                    "link": ""
+                },
+            ],
+            "footer": null
+        },
+        "help": {
+            "title": "help",
+            "header": null,
+            "list": [
+                {
+                    "title": "KNOWLEDGEBASE",
+                    "link": ""
+                },
+                {
+                    "title": "BLOG",
+                    "link": ""
+                }
+            ],
+            "footer": {"title": "CONTACT US", "link": ""}
+        },
+        "BROWSE EVENTS": {
+            "title": "BROWSE EVENTS",
+            "header": "",
+            "list": [],
+            "footer": ""
+        },
+        "account": {
+            "title": "CUSTOMER@gmail.com",
+            "header": {"title": "My Tickets", "link": ""},
+            "list": [
+                {
+                    "title": "Create Event",
+                    "link": ""
+                },
+                {
+                    "title": "saved events",
+                    "link": ""
+                },
+                {
+                    "title": "EARN COmission",
+                    "link": ""
+                },
+                {
+                    "title": "My Account",
+                    "link": ""
+                }
+            ],
+            "footer": {"title": "Log Out", "link": ""}
+        }
+    }
+};
