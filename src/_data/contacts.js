2fcd55a77baf217aa8f88c7f8b8f81a84c20b004
k.dubov <example@example.com>
2023-02-23 20:48:44 +0300

    add footer

---
+++ src/_data/contacts.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "email": "support@quicket.co.za",
+    "tel": "+27 21 424 9308",
+    "socials": {
+        "linkedin": {icon: "", link: ""},
+        "twitter": {icon: "", link: ""},
+        "insta": {icon: "", link: ""},
+        "facebook": {icon: "", link: ""},
+    }
+};
