b3c50cb77bf50bb8de4de7a351458a3b8bf1d4f2
k.dubov <example@example.com>
2023-03-01 18:23:21 +0300

    add contacts icons

--- src/_data/contacts.js
+++ src/_data/contacts.js
@@ -1,10 +1,15 @@
+const {facebookIcon} = require("../_includes/icons");
+const {instagramIcon} = require("../_includes/icons");
+const {twitterIcon} = require("../_includes/icons");
+const {linkedinIcon} = require("../_includes/icons");
+
 module.exports = {
     "email": "support@quicket.co.za",
     "tel": "+27 21 424 9308",
     "socials": {
-        "linkedin": {icon: "", link: ""},
-        "twitter": {icon: "", link: ""},
-        "insta": {icon: "", link: ""},
-        "facebook": {icon: "", link: ""},
+        "linkedin": {icon: linkedinIcon({className: "icon"}), link: ""},
+        "twitter": {icon: twitterIcon({className: "icon"}), link: ""},
+        "insta": {icon: instagramIcon({className: "icon"}), link: ""},
+        "facebook": {icon: facebookIcon({className: "icon"}), link: ""},
     }
 };
